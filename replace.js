276921ef558f8254d9a8c4ceeaa76043d32d7ec3
detailcore <savemysoulok@yandex.ru>
2023-12-06 12:29:16 +0400

    add build command

--- replace.js
+++ replace.js
@@ -6,7 +6,7 @@ const __filename = path.resolve(fileURLToPath(import.meta.url))
 const __dirname = path.resolve(path.dirname(__filename))
 const dist = path.resolve(__dirname + '/dist') // папка "D:\domains\portfolio.ai\.projects\vue3-table\dist" без слеша на конце
 
-const arrPaths = __dirname.split('\\')
+const arrPaths = __dirname.split(path.sep)
 const curDirName = arrPaths[arrPaths.length - 1] // "vue3-table"
 const preCurDirName = arrPaths[arrPaths.length - 2] // ".projects"
 // const curDirName = path.basename(path.resolve()) // "vue3-table"
@@ -20,11 +20,7 @@ const pathOldCss = data.match(/href="(.*?)"/)[1] // "/assets/index-1da8cd0c.css"
 data = data.replace(pathOldJs, `/${preCurDirName}/${curDirName}/dist` + pathOldJs)
 data = data.replace(pathOldCss, `/${preCurDirName}/${curDirName}/dist` + pathOldCss)
 
-console.log('arrPaths =>', arrPaths)
-console.log('curDirName =>', curDirName)
-console.log('preCurDirName =>', preCurDirName)
-
-// console.log('data', data) // Отобразить результат
-// fs.writeFileSync(indexOrig, data) // Перезаписываем данные в файле
+console.log('data', data) // Отобразить результат
+fs.writeFileSync(indexOrig, data) // Перезаписываем данные в файле
 
 console.log('Готово!')
